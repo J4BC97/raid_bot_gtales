d0eb6e8ac8ad5c8957830dbe4483377faf244005
J4BC97 <hammerban123@gmail.com>
2025-02-26 15:13:52 -0500

    Actualizacion: Guardian Tales Bot Script - RAID Helper (Gtales.top API)

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const { loadEvents } = require('./utils/eventLoader');
-const http = require('http'); // Importar el módulo http
+const http = require('http');
 
-// Crear una nueva instancia del cliente de Discord
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -12,30 +11,24 @@ const client = new Client({
     ],
 });
 
-// Cargar eventos
 loadEvents(client);
 
-// Crear un servidor HTTP básico
 const server = http.createServer((req, res) => {
-    // Ruta para UptimeRobot
     if (req.url === '/ping') {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('pong\n'); // Responde con "pong" cuando se accede a /ping
+        res.end('pong\n');
         return;
     }
 
-    // Ruta principal
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Bot de Discord está en funcionamiento.\n');
 });
 
-// Escuchar en el puerto proporcionado por Render
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
 
-// Iniciar sesión en Discord con el token
 client.login(process.env.DISCORD_BOT_TOKEN)
     .then(() => {
         console.log('Bot iniciado correctamente.');
