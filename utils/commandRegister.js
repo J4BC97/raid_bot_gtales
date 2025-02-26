bdcb6190406afc6a8b046dac756fb26b8fae06be
J4BC97 <hammerban123@gmail.com>
2025-02-26 15:17:05 -0500

    Actualizacion: Guardian Tales Bot Script - RAID Helper (Gtales.top API)

--- utils/commandRegister.js
+++ utils/commandRegister.js
@@ -1,22 +1,15 @@
-const { REST, Routes } = require('discord.js');
-const { loadCommands } = require('./commandLoader'); // Importa el cargador de comandos
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_BOT_TOKEN);
 
-module.exports = {
-    async registerCommands(client) {
-        const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_BOT_TOKEN);
+try {
+    console.log('Started refreshing application (/) commands.');
 
-        try {
-            console.log('Started refreshing application (/) commands.');
+    const commands = loadCommands();
+    await rest.put(
+        Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+        { body: commands.map(command => command.data.toJSON()) },
+    );
 
-            const commands = loadCommands(); // Carga los comandos
-            await rest.put(
-                Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-                { body: commands.map(command => command.data.toJSON()) },
-            );
-
-            console.log('Successfully reloaded application (/) commands.');
-        } catch (error) {
-            console.error(error);
-        }
-    },
-};
+    console.log('Successfully reloaded application (/) commands.');
+} catch (error) {
+    console.error(error);
+}
